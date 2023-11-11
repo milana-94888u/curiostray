4fa8a2eb7feb265ef721511e3b40a35f73e14076
Milana <94888u@gmail.com>
2023-11-11 12:01:09 +0100

    prepare for crafting

---
+++ src/resources/crafting/crafring_recipe.gd
@@ -0,0 +1,7 @@
+class_name CraftingRecipe
+extends Resource
+
+
+@export var ingridients: Array[CrafringIngridient]
+@export var result_item: Item
+@export var result_amount := 1
---
+++ src/resources/crafting/crafting_ingridient.gd
@@ -0,0 +1,6 @@
+class_name CrafringIngridient
+extends Resource
+
+
+@export var item: Item
+@export var required_amount := 1

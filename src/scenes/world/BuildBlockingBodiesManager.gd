389cac86674335b34ad4e47e46a61ca3c922e86e
Milana <94888u@gmail.com>
2023-11-13 20:08:36 +0100

    add generating building

--- src/scenes/world/BuildBlockingBodiesManager.gd
+++ src/scenes/world/BuildBlockingBodiesManager.gd
@@ -15,7 +15,7 @@ func shape_to_tiles(shape: CollisionShape2D) -> void:
 	var shape_relative := world.to_local(shape.global_position)
 	var shape_rect := shape.shape.get_rect()
 	var topleft := world.local_to_map(shape_relative + shape_rect.position)
-	var bottomright := world.local_to_map(shape_relative + shape_rect.end)
+	var bottomright := world.local_to_map(shape_relative + shape_rect.end - Vector2.ONE)
 	for x in range(topleft.x, bottomright.x + 1):
 		for y in range(topleft.y, bottomright.y + 1):
-			world.set_cell(0, Vector2i(x, y), 0, Vector2.ZERO)
+			world.set_block(x, y, world.BlockType.GOLD)
--- src/scenes/world/WorldGenerator.gd
+++ src/scenes/world/WorldGenerator.gd
@@ -17,10 +17,11 @@ func _ready() -> void:
 		await world.ready
 	generate_level()
 	generate_mineable()
+	set_main_building()
 
 
 func generate_level():
-	for x in range(width):
+	for x in range(-width, width):
 		var offset := roundi(terrain_noise.get_noise_1d(x) * 5)
 		world.set_block(x, offset, world.BlockType.COPPER)
 		for y in range(offset + 1, offset + 15):
@@ -31,7 +32,7 @@ func generate_level():
 
 
 func generate_mineable():
-	for x in range(width):
+	for x in range(-width, width):
 		for y in range(depth):
 			if world.get_cell_atlas_coords(0, Vector2i(x, y)) == Vector2i.ZERO:
 				var relative_depth := float(y) / float(depth)
@@ -39,3 +40,33 @@ func generate_mineable():
 				var cell_value := (ores_noise.get_noise_2d(x, y)) * relative_depth
 #				print(cell_value)
 				world.set_block(x, y, floori(cell_value * 6.0))
+
+
+func set_main_building() -> void:
+	var building := preload("res://src/scenes/world/building.tscn").instantiate() as Area2D
+	var left_offset := roundi(terrain_noise.get_noise_1d(-4) * 5)
+	var right_offset := roundi(terrain_noise.get_noise_1d(3) * 5)
+	var total_level := int(float(left_offset + right_offset) * 0.5) # mean
+	for x in range(-4, 4):
+		var offset := roundi(terrain_noise.get_noise_1d(x) * 5)
+		for y in range(offset, total_level):
+			world.erase_cell(0, Vector2(x, y))
+		for y in range(total_level, offset + 15):
+			world.set_block(x, y)
+	building.position = world.map_to_local(Vector2i(0, total_level - 3)) - 8 * Vector2.ONE
+	world.add_child(building)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	

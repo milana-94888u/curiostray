588bc545a83fda32f8cf9fb88082f23390202535
Milana <94888u@gmail.com>
2023-11-13 19:18:31 +0100

    add build blocker

---
+++ src/utils/build_blocker.gd
@@ -0,0 +1,9 @@
+class_name BuildBlocker
+extends Node
+
+
+func _ready() -> void:
+	var collision_shape := get_parent() as CollisionShape2D
+	if not collision_shape.is_node_ready():
+		await collision_shape.ready
+	BlockBreakManager.blocking_body_set.emit(collision_shape)

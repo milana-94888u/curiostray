2c4add8b9145b3143990332fc3b2c914c9e71343
Milana <94888u@gmail.com>
2023-12-01 22:11:40 +0100

    add labels on level upgrade

--- src/scenes/world/PlayerSpawner.gd
+++ src/scenes/world/PlayerSpawner.gd
@@ -16,6 +16,10 @@ var player: Player
 
 func spawn_player(player_level: int) -> void:
 	if is_instance_valid(player):
+		$CanvasLayer/NewLevelLabel.text = "Upgraded to level %d" % player_level
+		var tween := get_tree().create_tween().bind_node(self)
+		tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 1.0, 0.5)
+		tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 0.0, 0.5)
 		player.queue_free()
 	player = player_levels[player_level - 1].instantiate() as Player
 	player.position = world.map_to_local(Vector2i(0, init_level)) - Vector2(0, 6 + 6 * player_level)

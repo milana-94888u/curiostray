35eeaeaa3ba4b9c9208117888412027477084081
Milana <94888u@gmail.com>
2023-12-01 22:45:39 +0100

    fix level labe

--- src/scenes/world/PlayerSpawner.gd
+++ src/scenes/world/PlayerSpawner.gd
@@ -15,16 +15,17 @@ var player: Player
 
 
 func spawn_player(player_level: int) -> void:
+	$CanvasLayer/NewLevelLabel.text = "Player level level %d" % player_level
+	var tween := get_tree().create_tween().bind_node(self)
+	tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 1.0, 0.5)
+	tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 0.0, 0.5)
 	if is_instance_valid(player):
-		$CanvasLayer/NewLevelLabel.text = "Upgraded to level %d" % player_level
-		var tween := get_tree().create_tween().bind_node(self)
-		tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 1.0, 0.5)
-		tween.tween_property($CanvasLayer/NewLevelLabel, "modulate:a", 0.0, 0.5)
 		player.queue_free()
 	player = player_levels[player_level - 1].instantiate() as Player
 	player.position = world.map_to_local(Vector2i(0, init_level)) - Vector2(0, 6 + 6 * player_level)
 	world.add_child(player)
-	player.player_data.no_energy.connect(respawn_player.bind(player_level))
+	if not player.player_data.no_energy.is_connected(respawn_player):
+		player.player_data.no_energy.connect(respawn_player.bind(player_level))
 
 
 func respawn_player(player_level: int) -> void:
